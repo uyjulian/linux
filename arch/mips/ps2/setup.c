a978213ca4bab47f74618109dfa282b91479eddd
Rick Gaiser <rgaiser@gmail.com>
2016-01-14 19:34:01 +0100

    ps2: change irx modules to a working combination

--- arch/mips/ps2/setup.c
+++ arch/mips/ps2/setup.c
@@ -239,6 +239,9 @@ static int __init ps2_board_setup(void)
 	if (ps2_pccard_present == 0x0200) {
 		pr_info("Playstation 2 SLIM\n");
 
+		if (load_module_firmware("ps2/ps2dev9.irx", 0) < 0)
+			pr_err("loading ps2/ps2dev9.irx failed\n");
+
 		if (load_module_firmware("ps2/intrelay-dev9-rpc.irx", 0) < 0)
 			pr_err("loading ps2/intrelay-dev9-rpc.irx failed\n");
 
@@ -247,8 +250,8 @@ static int __init ps2_board_setup(void)
 	else {
 		pr_info("Playstation 2 FAT\n");
 
-		if (load_module_firmware("ps2/intrelay-dev9.irx", 0) < 0)
-			pr_err("loading ps2/intrelay-dev9.irx failed\n");
+		if (load_module_firmware("ps2/intrelay-direct.irx", 0) < 0)
+			pr_err("loading ps2/intrelay-direct.irx failed\n");
 
 		if (ps2_pccard_present == 0x0100) {
 			pr_info(" - With network adapter\n");
